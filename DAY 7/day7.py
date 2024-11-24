9a3908d5cdc6d62022097c3f1fd3b0be9c0a562c
asawari01 <111641926+asawari01@users.noreply.github.com>
2024-11-24 21:59:39 +0530

    Add files via upload

---
+++ DAY 7/day7.py
@@ -0,0 +1,30 @@
+m, n = map(int, input("Enter rows and columns: ").split())
+
+mat = []
+
+for i in range(m):
+    mat.append(list(map(int, input().split())))
+
+zero_col = set()
+zero_row = set()
+
+for i in range(m):
+    for j in range(n):
+
+        if mat[i][j] == 0:
+            zero_row.add(i)
+            zero_col.add(j)
+
+for i in zero_row:
+    for j in range(n):
+        mat[i][j] = 0
+
+for j in zero_col:
+    for i in range(m):
+        mat[i][j] = 0
+
+print("Updated Matrix:")
+for row in mat:
+    print(*row)
+
+
